23f517fccec7a6538cde5e3568da2de32dbdf9f1
Kitti Pariyaakkarakun <zatera1920@gmail.com>
2015-07-04 13:49:22 +0700

    Check error and post method

--- web.go
+++ web.go
@@ -8,14 +8,13 @@ import(
 )
 
 func main() {
-	// (response,request)
-	// POST /contacts
-	// { "name" :"",tel: "",email:"" }
-	// contacts := []contact.Contact{}
 	http.HandleFunc("/contacts",func(q http.ResponseWriter,r *http.Request) {
 		var c contact.Contact
-		json.NewDecoder(r.Body).Decode(&c)
-		fmt.Println(q)
+		err := json.NewDecoder(r.Body).Decode(&c)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(c)
 		})
 	http.ListenAndServe(":3000",nil)
 }
