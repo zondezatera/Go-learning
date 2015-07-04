45c08dcf3c8902a8407a09d9aa08934f75ae25ff
Kitti Pariyaakkarakun <zatera1920@gmail.com>
2015-07-04 10:26:53 +0700

    init

---
+++ hello.go
@@ -0,0 +1,13 @@
+// package for run core program
+package main
+// include other package 
+import "fmt"
+// var [name] [type] = value
+	var s = "Hello Golang"
+// func == function
+// core function 
+func main() {
+	s = 1
+	// first letter uppercase meaning public var
+	fmt.Println(s)
+}
