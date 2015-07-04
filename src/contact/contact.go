e22f3be2457e4ea4c03c75d5b9a8a5422e6ac386
Kitti Pariyaakkarakun <zatera1920@gmail.com>
2015-07-04 13:40:21 +0700

    Update get method

--- src/contact/contact.go
+++ src/contact/contact.go
@@ -5,6 +5,7 @@ import "fmt"
 type Contact struct {
 	Name string
 	Tel string
+	Email string
 }
 
 func Say(c Contact) string {
--- web.go
+++ web.go
@@ -1,11 +1,21 @@
 package main
 
-import "net/http"
+import(
+	"contact"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	// (response,request)
-	http.HandleFunc("/",func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello Gopher"))
-	})
+	// POST /contacts
+	// { "name" :"",tel: "",email:"" }
+	// contacts := []contact.Contact{}
+	http.HandleFunc("/contacts",func(q http.ResponseWriter,r *http.Request) {
+		var c contact.Contact
+		json.NewDecoder(r.Body).Decode(&c)
+		fmt.Println(q)
+		})
 	http.ListenAndServe(":3000",nil)
 }
