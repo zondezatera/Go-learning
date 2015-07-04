db1a212454a85ce480ec0ca0c3742775ed2597e6
Kitti Pariyaakkarakun <zatera1920@gmail.com>
2015-07-04 10:34:38 +0700

    Float and int

--- hello.go
+++ hello.go
@@ -7,7 +7,8 @@ import "fmt"
 func main() {
 // var [name] [type] = value
 // s: value
-	var b bool = true // false
+	var a float32 = 1.23 
+	var b int = 1.23 
 	// first letter uppercase meaning public var
 	fmt.Println(s)
 }
